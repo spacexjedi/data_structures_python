259cb3dbaf83c57b1c04c185671337281660f766
spacexjedi <58277328+spacexjedi@users.noreply.github.com>
2020-07-24 14:24:00 -0300

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,5 +3,14 @@ import matplotlib.pyplot as plt
 x = [1,2,3]
 y = [6,7,8]
 
+z = [1,2,3]
+w = [6,7,8]
+
+plt.title("Deaths by mms")
+plt.xlabel("axis x")
+plt.ylabel("axis y")
 plt.plot(x,y)
+plt.bar(x,y)
+plt.bar(x,y, label="orange bars")
+plt.bar(z,w, label="blue bars")
 plt.show()
